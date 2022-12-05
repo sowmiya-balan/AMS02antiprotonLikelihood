a5517a72a074319babfafd11f1a3993d66834e92
Balan Sowmiya <sbalan@ttpgalba.ttp.kit.edu>
2022-12-05 13:17:33 +0100

    Parallel likelihood computation

--- DRN_interface.py
+++ DRN_interface.py
@@ -5,6 +5,7 @@ from preamble import *
 
 class DRNet:
     def __init__(self,propagation_parameters,propagation_model,prevent_extrapolation,data,errors,cov_inv):
+        print("Initializing DRN")
         self.cov_inv = cov_inv
         self.errors = errors
         self.data = data
@@ -12,19 +13,22 @@ class DRNet:
         model_options = ['run1', 'DIFF.BRK', 'INJ.BRK+vA']
         solar_modulation_options = {'run1':self.solar_mod_run1, 'DIFF.BRK':self.solar_mod_brk, 'INJ.BRK+vA':self.solar_mod_brk}
         if propagation_model in model_options:
+            print("valid pm")
             self.propagation_model = propagation_model
-            self.dep_path = script_directory + '/dependencies/' + self.propagation_model + '/'
-            self.solar_modulation = solar_modulation_options[self.propagation_model]
         else:
             print()
             print('The propagation model "%s" is not provided in this tool. It will be set to default (DIFF.BRK).'%propagation_model)
             self.propagation_model = 'DIFF.BRK'
+        self.dep_path = script_directory + '/dependencies/' + self.propagation_model + '/'
+        self.solar_modulation = solar_modulation_options[self.propagation_model]
         self.load_deps()
         self.load_pp_data()
         self.preprocessing_prop_params(propagation_parameters)
         self.phi_CR_LIS = self.CR_sim()
+        # print('phiCRLIS',self.phi_CR_LIS.shape)
         self.phi_CR   = self.solar_modulation(self.phi_CR_LIS) 
         self.chi2_CR_uncorr = self.chi2(self.phi_CR)
+        # print('cuncorr',self.chi2_CR_uncorr)
         self.chi2_CR_corr = self.chi2_cov(self.phi_CR)
         self.chi2_CR_diff = np.clip(self.chi2_CR_uncorr - self.chi2_CR_corr,  -500,500)
         self.CR_marginalized_likelihood = np.sum(np.exp(self.chi2_CR_diff/2),axis=-1)
@@ -60,6 +64,8 @@ class DRNet:
     # propagation_parameters - (n,N_pp) shaped array if flux is predicted for n DM_masses
     def preprocessing_prop_params(self, propagation_parameters):
         # Converting propagation parameters to 2D array. Required transformations will be done within simulations (DM_sim, CR_sim)
+        if type(propagation_parameters) == list :
+            propagation_parameters = np.array(propagation_parameters)
         if propagation_parameters.ndim == 1:
             propagation_parameters = propagation_parameters[np.newaxis,:]
         # Checking if given propagation parameters are custom or dummy
@@ -69,7 +75,7 @@ class DRNet:
         # Checking if correct number of propagation parameters are given
         elif propagation_parameters.shape[1] != self.N_pp :
             print('The number of propagation parameters is not consistent with the propagation model. The default multinest sample will be used for marginalization.')
-            self.pp = self.mnpp    
+            self.pp = self.mnpp  
             self.marginalization = True 
         # Checking if the given propagation parameters are inside the region in which the network is trained
         elif self.pe:
@@ -85,23 +91,29 @@ class DRNet:
                     self.marginalization = False
         else :
             self.pp = propagation_parameters
-            self.marginalization = False               
-        
+            self.marginalization = False   
+        self.pp_ur = self.pp
+        # print(np.shape(self.pp))  
     # DM_masses - 1D array of shape (n,)
     def preprocessing_DMparams(self, DM_mass, br_fr, sigma_v):
         # Preparing DM masses
         # Converting float DM mass to 1D array
         if type(DM_mass) == float or type(DM_mass)== np.float64 or type(DM_mass) == np.int64:
             DM_mass = np.array([DM_mass])
+        elif type(DM_mass) == list :
+            DM_mass = np.array(DM_mass)
         self.DM_mass = DM_mass
+        # print('lenDMmass',len(self.DM_mass))
+        self.DM_mass_r = np.repeat(DM_mass,len(self.pp),axis=0)
         # Min-max standardization of DM masses
         m_DM = ((np.log10(DM_mass)+3) - np.log10(5e3)) / (np.log10(5e6)-np.log10(5e3))
         # Repeating mass for the given number of propagation parameter points
-        m_DM = np.repeat(m_DM,len(self.pp),axis=0)
-        self.m_DM = m_DM
+        self.m_DM = np.repeat(m_DM,len(self.pp),axis=0)
 
         # Preparing branching fractions
         # Converting 1D bf array to 2D array
+        if type(br_fr)==list:
+            br_fr = np.array([br_fr])
         if br_fr.ndim == 1:
             br_fr = br_fr[np.newaxis,:] 
         # Replacing zeros by 1e-5 and renormalizing
@@ -118,6 +130,11 @@ class DRNet:
         bf = (np.log10(bf_temp) - np.array(self.DM_trafos[1,0])) / (np.array(self.DM_trafos[1,1])- np.array(self.DM_trafos[1,0])) 
         bf_r = np.repeat(bf,len(self.pp),axis=0)
         self.bf = bf_r
+
+        # Repeating propagation parameters and branching fractions if necessary
+        if len(self.DM_mass)>1:
+            self.pp = np.tile(self.pp,(len(self.DM_mass),1))
+            self.bf = np.repeat(bf_r,len(self.DM_mass),axis=0)
         
         # Preventing extrapolation
         self.stop_sim = False
@@ -139,6 +156,8 @@ class DRNet:
         # Converting float sigma_v to 1D array
         if type(sigma_v) == float or type(sigma_v)== np.float64 or type(sigma_v) == np.int64:
             sigma_v = np.array([sigma_v])
+        if type(sigma_v) == list:
+            sigma_v = np.array(sigma_v)
         self.sv = sigma_v
         
     # Processing inputs, obtaining prediction from DMNet, and postprocessing prediction:
@@ -151,9 +170,9 @@ class DRNet:
         else: 
             min_x = -0.1 # Necessary for model without reacceleration (DM antiproton spectra diverge for E -> m_DM, x -> 0)
         x_temp = 10**(np.linspace(-3.7, min_x, 40))
-        x = np.repeat([x_temp],len(self.m_DM),axis=0)
+        x = np.repeat([x_temp],len(self.DM_mass_r),axis=0)
         # E_dmnet - (n,40) array of kinetic energy(KE) per nucleon values at which network predicts output
-        E_dmnet = x*self.DM_mass[:,None]
+        E_dmnet = x*self.DM_mass_r[:,None]
         # y_DM_x - (n,40) array of y values predicted by the DMNet for different x values; y(x) = log10(m^3 x phi(E))        
         # DM_model = tf.keras.models.load_model(self.DM_model)
         DM_model = tf.keras.models.load_model(self.dep_path + 'DM_model_x.h5')
@@ -171,21 +190,22 @@ class DRNet:
         del DM_model    
         gc.collect()
         # phi_dmnet - (n,40) array of flux values predicted by the DMNet for different KE per nucleon values
-        phi_dmnet = 10**(y_DM_x) / (self.DM_mass[:,None]**3 * x)
+        phi_dmnet = 10**(y_DM_x) / (self.DM_mass_r[:,None]**3 * x)
         # phi_DM_LIS - (n,28) array of flux values interpolated to obtain fluxes at the same KE per nucleon values as in E_drn so that it can
         # be added to phi_CR_LIS. Only after solar modulation, the flux is to be interpolated to E_ams values.
-        phi_DM_LIS = np.zeros((len(self.m_DM),len(E_drn)))
-        E_drn_allowed = []
-        indices = []
+        phi_DM_LIS = np.zeros((len(self.DM_mass_r),len(E_drn)))
         # Flux predicted by DMNet is only reliable in the allowed x range, i.e. for only those KE per nucleon in E_drn 
         # which for a given DM mass fall within the allowed x*m values. Thus we make a list of these allowed E_drn values, interpolate
         # the flux only at these values and set all other flux values at other E_drn to zero.
-        for j in range(len(E_drn)):
-            if E_drn[j]/self.DM_mass >= 10**(-3.7) and E_drn[j]/self.DM_mass <= 1:
-                E_drn_allowed.append(E_drn[j])
-                indices.append(j)
-        for i in range(len(self.m_DM)):
-            phi_DM_LIS[i,indices]  = np.exp(np.interp(np.log(E_drn_allowed), np.log(E_dmnet[i]), np.log(phi_dmnet[i])) )
+        for i in range(len(self.DM_mass_r)):
+            E_drn_allowed = []
+            indices = []
+            for j in range(len(E_drn)):
+                if E_drn[j]/self.DM_mass_r[i] >= 10**(-3.7) and E_drn[j]/self.DM_mass_r[i] <= 1:
+                    E_drn_allowed.append(E_drn[j])
+                    indices.append(j)
+            phi_DM_LIS[i,indices]  = np.exp(np.interp(np.log(E_drn_allowed), np.log(E_dmnet[i]), np.log(phi_dmnet[i])))
+        # phi_DM_LIS = np.reshape(phi_DM_LIS,(len(self.DM_mass),len(self.pp_ur),len(E_drn)))
         # print('Interpolated DMNet Output:',phi_DM_LIS)
         return phi_DM_LIS
         # Outputs 2D phi_DM_LIS - (n,58) array of flux values interpolated to obtain fluxes at the same KE per nucleon values as in E_drn
@@ -215,7 +235,9 @@ class DRNet:
             print('The DM antiproton flux cannot be predicted by DRN due to atleast one parameter outside the region in which the network is trained.')
         else:
             DRN_output = self.DM_sim()
-            phi_DM_LIS = self.sv[:,None]/10**(-25.5228)*DRN_output
+            phi_DM_LIS = np.array([self.sv[i]/10**(-25.5228)*DRN_output for i in range(len(self.sv))])
+            # print('phidmlis:',phi_DM_LIS)
+            phi_DM_LIS = np.reshape(phi_DM_LIS,(len(self.sv)*len(self.DM_mass),len(self.pp_ur),len(E_drn)))
             # Outputs 2D phi_DM_LIS - (n,28) array of flux values interpolated to obtain fluxes at the same KE per nucleon values as in E_drn
         return phi_DM_LIS
 
@@ -227,7 +249,7 @@ class DRNet:
         else :
             return np.sum((diff[:,14:])**2 / self.errors[14:]**2,axis = -1)
 
-    # Calculates chi2 using ams data, covariane matrix and predicted flux
+    # Calculates chi2 using ams data, covariance matrix and predicted flux
     def chi2_cov(self,phi_pred):
         diff = self.data-phi_pred
         if phi_pred.ndim == 1:
@@ -344,20 +366,33 @@ class DRNet:
 
     def TOA_sim(self, phi_DM_LIS):
         phi_LIS = self.phi_CR_LIS + phi_DM_LIS
-        phi_DMCR = self.solar_modulation(phi_LIS) 
+        # print('phi_LIS:',phi_LIS)
+        phi_DMCR = np.array([self.solar_modulation(phi_LIS[i]) for i in range(len(phi_LIS))])
+        # print('phi_DMCR',phi_DMCR)
         return phi_DMCR
     
     def del_chi2(self, phi_DMCR):
-        chi2_DMCR = self.chi2(phi_DMCR)
-        chi2_diff = np.clip(self.chi2_CR_uncorr - chi2_DMCR,  -500,500)
-        del_chi2 = np.log( 1/len(self.pp) * np.sum(np.exp(chi2_diff/2),axis=-1) )
+        # print('phiDMCR:',phi_DMCR)
+        del_chi2 = []
+        for i in range(len(phi_DMCR)):
+            # print('phiDMCRi:',phi_DMCR[i])
+            chi2_DMCR = self.chi2(phi_DMCR[i])
+            # print('chisquares:',chi2_DMCR)
+            chi2_diff = np.clip(self.chi2_CR_uncorr - chi2_DMCR,  -500,500)
+            # print('chi2_diff:',chi2_diff)
+            del_chi2_t = np.log( 1/len(self.pp_ur) * np.sum(np.exp(chi2_diff/2),axis=-1) )
+            # print('del_chi2_t:',del_chi2_t)
+            del_chi2.append(del_chi2_t)
         return del_chi2
 
     def del_chi2_corr(self, phi_DMCR):
-        chi2_DMCR = self.chi2_cov(phi_DMCR)   
-        chi2_DM_diff = np.clip(self.chi2_CR_uncorr - chi2_DMCR,  -500,500)
-        delta_chi2 = np.log(np.sum(np.exp(chi2_DM_diff/2),axis=-1) / self.CR_marginalized_likelihood)
-        return delta_chi2
+        delta_chi2_cov = []
+        for i in range(len(phi_DMCR)):
+            chi2_DMCR = self.chi2_cov(phi_DMCR[i])
+            chi2_DM_diff = np.clip(self.chi2_CR_uncorr - chi2_DMCR,  -500,500)
+            delta_chi2_t = np.log(np.sum(np.exp(chi2_DM_diff/2),axis=-1) / self.CR_marginalized_likelihood)
+            delta_chi2_cov.append(delta_chi2_t)
+        return delta_chi2_cov
 
     # # theta_prop - (number_of_m_DM,11) array containing generally accepteed values of propagation parameters for run1
     # def usual_theta_prop(number_of_m_DM,gamma_1p = 1.80, gamma_1 = 1.79, gamma_2p = 2.405, gamma_2 = 2.357, R_0 = 7.92e3, s = 0.37, D_0 = 2.05e28, delta = 0.419, v_alfven = 8.84, v_0c = 0.09, z_h = 2.60):
--- check.py
+++ check.py
@@ -4,16 +4,17 @@ from DRN_interface import *
 from pbarlike import *
 
 #%% Inputs
-m_DM = np.array([3000.])
-bf = np.array([0,0,0,0,1,0,0,0])
-sv = 3e-26
+m_DM = np.array([100.,100.,100.]) #np.array([3000.,300.]) #
+bf = np.array([0.00000000e+00, 1.02133998e-06, 2.40315289e-03, 0.00000000e+00,  7.41973454e-01, 2.55334994e-01, 0.00000000e+00, 0.00000000e+00])
+sv = [1.70104582586906e-31,1.70104582586906e-31]
 pp_run1 = np.array([1.8, 1.79, 2.405, 2.357, 7.92e+03, 0.37, 2.05e+28, 0.419, 8.84, 0.09, 2.60])
 pp_db = np.array([2.34, 2.28, 3.63e+28, -0.66, 0.52, -0.15, 3.83e+3, 0.4, 2.05e+5])
-pm = "run1"
-prevent_extrapolation = False
+pm = "DIFF.BRK"
+prevent_extrapolation = True
 
 #%% Results
 print('Calling pbarlike...')
-results = py_pbarlike(m_DM, bf,pp_db,sv,propagation_model=pm,prevent_extrapolation = prevent_extrapolation)
+results = py_pbarlike(m_DM, bf,pp_run1,sv,propagation_model=pm,prevent_extrapolation = prevent_extrapolation)
 print('del_chi2 = ', results["uncorrelated"])
 print('del_chi_cov = ', results["correlated"])
+print('nla',bbBar_grid(2,sv))
