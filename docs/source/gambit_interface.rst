057062e9184d57f1ea8792890c8cc8b341d73a9f
sowmiya-balan <sowmiya.balan@rwth-aachen.de>
2023-02-15 20:15:13 +0100

    Adding code

---
+++ docs/source/gambit_interface.rst
@@ -0,0 +1,9 @@
+gambit\_interface module
+------------------------
+
+Module containing helper functions for interface with GAMBIT.
+
+.. automodule:: pbarlike.gambit_interface
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ pbarlike/__init__.py
@@ -0,0 +1,7 @@
+import os
+script_directory = os.path.dirname(os.path.realpath(__file__))
+m_p = 0.9382720881604903  # Mass of proton in GeV (938.2720881604903 MeV)
+
+from pbarlike.convenience_functions import loglike_recipe
+from pbarlike import banner
+
